058ba8964859e2b1bf70a013867e5257d3d07c6b
Yurui Ming <yrming@gmail.com>
2021-10-14 17:23:24 +1100

    add materials for lab-3

--- lab-3/ic_data.py
+++ lab-3/ic_data.py
@@ -1,11 +1,44 @@
 import os
 import numpy as np
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
+import sonnet as snt
 
-num_images = 64
-image_width = 28
-image_height = 28
+class Input(snt.AbstractModule):
+    def __init__(self, batch_size, image_dims, num_epochs = -1, name = 'input'):
+        super(Input, self).__init__(name = name)
+        self._batch_size = batch_size
+        self._image_dims = image_dims
+        self._num_epochs = num_epochs
+
+    def _parse_function(self, example):
+        dims = np.prod(self._image_dims)
+
+        features = {
+            "image": tf.FixedLenFeature([dims], dtype = tf.float32),
+            "label": tf.FixedLenFeature([], dtype = tf.int64)
+        }
+
+        example_parsed = tf.parse_single_example(serialized = example, features = features)
+        value = tf.reshape(example_parsed['image'], self._image_dims)
+
+        label = example_parsed['label']
+
+        return value, label
+        
+    def _build(self, filename):
+        assert os.path.isfile(filename), "invalid file name: {}".format(filename)
+
+        dataset = tf.data.TFRecordDataset([filename])
+        dataset = dataset.map(self._parse_function)
+
+        dataset = dataset.batch(self._batch_size)
+        dataset = dataset.repeat(self._num_epochs)
+
+        it = dataset.make_one_shot_iterator()
+        images, labels = it.get_next()
+
+        return images, labels
+
 
 def draw_image(images, rows, cols, tensor_name = "images"):
     import tfmpl
@@ -42,27 +75,24 @@ def draw_image(images, rows, cols, tensor_name = "images"):
 
     return image_str
 
-
-if not os.path.exists("output-data"):
-    os.makedirs("output-data")
     
-writer = tf.summary.FileWriter("output-data", tf.get_default_graph())    
-
-if not os.path.exists("data/MNIST/"):
-    os.makedirs("data/MNIST/")
-
-data = input_data.read_data_sets("data/MNIST/", one_hot=True)
-images = tf.constant(data.test.images[0:num_images])
-images = tf.reshape(images, (num_images, image_height, image_width))
+if __name__ == '__main__':
+    num_images = 64
+    image_width = 28
+    image_height = 28   
+ 
+    input_ = Input(num_images, [image_height, image_width, 1])
+    images, labels = input_('data/mnist/mnist-train.tfr')
     
-with tf.Session() as sess:
-    sess.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
-
-    print(sess.run(tf.shape(images)))
+    if not os.path.exists("output-data"):
+        os.makedirs("output-data")
         
-    image_str = draw_image(images, 8, 8)
-    writer.add_summary(image_str, global_step = 0)
-
-
+    writer = tf.summary.FileWriter("output-data", tf.get_default_graph())
+        
+    with tf.Session() as sess:
+        sess.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
+            
+        image_str = draw_image(images, 8, 8)
+        writer.add_summary(image_str, global_step = 0)
 
 
